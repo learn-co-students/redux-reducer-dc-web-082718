a37f64527bc6ab701169e4b249059b7139b90e7f
Angelica Winkler <carowink3@gmail.com>
2018-11-13 20:46:25 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
 export function manageFriends(state, action){
+  switch (action.type){
+    case "ADD_FRIEND":
+      return {...state, friends: [...state.friends, action.friend]};
+    case "REMOVE_FRIEND":
+      const friendIndex = state.friends.findIndex(friend => friend.id === action.id)
+
+      var firstHalf = state.friends.slice(0, friendIndex);
+      var secondHalf = state.friends.slice(friendIndex + 1);
+
+      return {...state, friends: [...firstHalf, ...secondHalf]}
+    default:
+      return state;
+  }
 }
